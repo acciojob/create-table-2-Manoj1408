147a3b21c9bdb0b46d94f4ddde403ccd64597169
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-24 21:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const tbody = document.getElementById("myTable");
 function createTable() {
     //Write your code here
+	let rows = prompt("Input number of rows");
+	let columns = prompt("Input number of columns");
+	for (let row = 0; row < rows; row++) {
+  let tr = document.createElement("tr");
+
+  for (let col = 0; col < columns; col++) {
+    let td = document.createElement("td");
+	  td.innerText = `Row-${row} Column-${col}`
+    tr.appendChild(td);
+  }
+  //append the row into the bodyw
+  tbody.appendChild(tr);
+}
+	
   
 }
